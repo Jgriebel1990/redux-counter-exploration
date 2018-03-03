f6e11534bdf3fc34afb609f4fcdd09a86e70f6f1
jefferson Griebel <jefferson.griebel@gmail.com>
2018-03-02 19:10:47 -0500

    reorganized to use Provider in index.js and used connect to provide state and dispatch to the app component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
 
-import { createStore } from 'redux';
-
-//initial state
-const initialState = 0;
-//reducer (how state changes)
-const countReducer = (state=initialState, action) => {
-  switch(action.type){
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    default:
-      return state
-  }
-};
-//creation of the store
-const store = createStore(
-  countReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
 
 class App extends Component {
   render() {
+    console.log(this);
     return (
       <div>
         <h1>
-          {store.getState()}
+          {this.props.count}
         </h1>
-        <button onClick={() => store.dispatch({type: 'INCREMENT'})}>+</button>
-        <button onClick={() => store.dispatch({type: 'DECREMENT'})}>-</button>
+        <button onClick={() => this.props.dispatch({type: 'INCREMENT'})}>+</button>
+        <button onClick={() => this.props.dispatch({type: 'DECREMENT'})}>-</button>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    count: state
+  }
+}
+
+export default connect(mapStateToProps, null)(App);
