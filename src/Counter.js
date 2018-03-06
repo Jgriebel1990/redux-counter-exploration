1c4ab51ce2f8663985aae4bcb6275caf1a810567
jefferson Griebel <jefferson.griebel@gmail.com>
2018-03-05 19:59:20 -0500

    deleted some more code!!!

--- src/Counter.js
+++ src/Counter.js
@@ -8,8 +8,6 @@ const Counter = props => {
         </h1>
         <button onClick={() => props.decrementBy(props.input)}>decrease by</button>
         <button onClick={() => props.incrementBy(props.input)}>increase by</button>
-        <button onClick={() => props.decrementBy(1)}>-</button>
-        <button onClick={() => props.incrementBy(1)}>+</button>
         <button onClick={() => props.incrementBy(2)}>+2</button>
         <button onClick={() => props.decrementBy(2)}>-2</button>
       </div>
