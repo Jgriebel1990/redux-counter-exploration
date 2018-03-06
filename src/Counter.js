b47af2d5de09a2f1ac3e9829904aabc839b465ce
jefferson Griebel <jefferson.griebel@gmail.com>
2018-03-05 20:36:19 -0500

    css

--- src/Counter.js
+++ src/Counter.js
@@ -6,7 +6,7 @@ const Counter = props => {
         <h1 className='text-clr'>
             {props.count}
         </h1>
-        <button className='btn-clr-1' onClick={() => props.decrementBy(props.input)}>decrease by</button>
+        <button className='btn-1' onClick={() => props.decrementBy(props.input)}>decrease by</button>
         <button className='btn-clr-2' onClick={() => props.incrementBy(props.input)}>increase by</button>
         <button className='btn-clr-3' onClick={() => props.incrementBy(2)}>+2</button>
         <button className='btn-clr-4' onClick={() => props.decrementBy(2)}>-2</button>
